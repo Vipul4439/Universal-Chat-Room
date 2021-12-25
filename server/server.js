2e184896aaf7d159b03cf3f93dd4986b1d485dae
Vipul <Vipulsoniab17@gmail.com>
2021-12-25 11:06:32 +0530

    add socket connnections configuration

--- server/server.js
+++ server/server.js
@@ -1,1 +1,27 @@
-console.log("Hello");
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const usersarr = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-entry", (nameOfUser) => {
+    usersarr[socket.id] = nameOfUser;
+
+    socket.broadcast.emit("user-name", nameOfUser);
+  });
+
+  socket.on("send", (message) => {
+    socket.broadcast.emit("showmsgtoall", {
+      message: message,
+      name: usersarr[socket.id],
+    });
+  });
+
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", usersarr[socket.id]);
+    delete usersarr[socket.id];
+  });
+});
